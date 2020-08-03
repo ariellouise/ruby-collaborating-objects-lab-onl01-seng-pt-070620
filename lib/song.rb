6df331d7751faf7a4054f4f712a959e589457349
ariel murphy <arielladders@gmail.com>
2020-08-03 23:37:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,19 @@
 class Song 
-  attr_accessor :name 
+  attr_accessor :name, :artist
+  
+  @@all = []
   
   def initialize(name)
     @name = name 
+    self.save
+  end 
+  
+  def save 
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all 
   end 
   
   def name 
