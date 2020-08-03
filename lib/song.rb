ab7ef077357e1c5054445c361ab6cd2bde34d2ec
ariel murphy <arielladders@gmail.com>
2020-08-03 23:47:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,7 +16,7 @@ class Song
     @@all 
   end 
   
-  def self.new_by_filename(filename)
+  def song.new_by_filename(filename)
     artist, song = filename.split(" - ")
     new_song = self.new(song)
     new_song.artist_name = artist 
