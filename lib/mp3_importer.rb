bfc57dc1a0dc39de860f5bee3c4fb9dd931e74d4
ariel murphy <arielladders@gmail.com>
2020-08-03 23:44:03 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,9 +1,12 @@
 class MP3Importer 
+  attr_reader :path 
   
-  def initialize 
+  def initialize(path)
+    @path = path 
   end 
   
   def files 
+    @files ||= Dir.glob("#{path}/*.mp3").collect{|x| x.gsub("#{path}/", "")}
   end 
   
   def import 
