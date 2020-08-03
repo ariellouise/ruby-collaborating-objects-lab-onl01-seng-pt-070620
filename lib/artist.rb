cfb5d11d69b8622190da9e1a518f00d7aca6f9b2
ariel murphy <arielladders@gmail.com>
2020-08-03 23:33:02 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,4 +4,16 @@ class Artist
   def initialize(name)
     @name = name 
   end 
+
+  def add_song 
+  end 
+  
+  def songs 
+  end 
+  
+  def find_or_create_by_name 
+  end 
+  
+  def print_songs 
+  end 
 end 
